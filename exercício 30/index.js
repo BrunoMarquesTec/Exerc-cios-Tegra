1f0303905de470d48aa2726a5113e6013159c1a6
Bruno Marques <brunocpv.tec@gmail.com>
2022-05-25 12:27:17 -0300

    Resolvido

---
+++ exercício 30/index.js
@@ -0,0 +1,324 @@
+function clicar(){
+
+    var numero = document.getElementById('num1').value;
+    var resp = document.getElementById('resposta');
+
+
+    switch(numero){
+
+        //de 0 a 10
+            case '0':
+            resp.innerHTML = 'Zero';
+            break;
+            case '1':
+            resp.innerHTML = 'Um';
+            break;
+            case '2':
+            resp.innerHTML = 'Dois';
+            break;
+            case '3':
+            resp.innerHTML = 'Três';
+            break;
+            case '4':
+            resp.innerHTML = 'Quatro';
+            break;
+            case '5':
+            resp.innerHTML = 'Cinco';
+            break;
+            case '6':
+            resp.innerHTML = 'Seis';
+            break;
+            case '7':
+            resp.innerHTML = 'Sete';
+            break;
+            case '8':
+            resp.innerHTML = 'Oito';
+            break;
+            case '9':
+            resp.innerHTML = 'Nove';
+            break;
+            case '10':
+            resp.innerHTML = 'Dez';
+            break;
+     //de 11 a 20
+            case '11':
+            resp.innerHTML = 'Onze';
+            break;
+            case '12':
+            resp.innerHTML = 'Doze';
+            break;
+            case '13':
+            resp.innerHTML = 'Treze';
+            break;
+            case '14':
+            resp.innerHTML = 'Quatroze';
+            break;
+            case '15':
+            resp.innerHTML = 'Quinze';
+            break;
+            case '16':
+            resp.innerHTML = 'Dezesseis';
+            break;
+            case '17':
+            resp.innerHTML = 'Dezessete';
+            break;
+            case '18':
+            resp.innerHTML = 'Dezoito';
+            break;
+            case '19':
+            resp.innerHTML = 'Dezenove';
+            break;
+            case '20':
+            resp.innerHTML = 'Vinte';
+            break;
+      //de 21 a 30
+            case '21':
+            resp.innerHTML = 'Vinte e um';
+            break;
+            case '22':
+            resp.innerHTML = 'Vinte e dois';
+            break;
+            case '23':
+            resp.innerHTML = 'Vinte e Três';
+            break;
+            case '24':
+            resp.innerHTML = 'Vinte e Quatro';
+            break;
+            case '25':
+            resp.innerHTML = 'Vinte e Cinco';
+            break;
+            case '26':
+            resp.innerHTML = 'Vinte e Seis';
+            break;
+            case '27':
+            resp.innerHTML = 'Vinte e Sete';
+            break;
+            case '28':
+            resp.innerHTML = 'Vinte e Oito';
+            break;
+            case '29':
+            resp.innerHTML = 'Vinte e Nove';
+            break;
+            case '30':
+            resp.innerHTML = 'Trinta';
+            break;
+     //de 31 a 40
+            case '31':
+            resp.innerHTML = 'Trinta e Um';
+            break;
+            case '32':
+            resp.innerHTML = 'Trinta e Dois';
+            break;
+            case '33':
+            resp.innerHTML = 'Trinta e Três';
+            break;
+            case '34':
+            resp.innerHTML = 'Trinta e Quatro';
+            break;
+            case '35':
+            resp.innerHTML = 'Trinta e Cinco';
+            break;
+            case '36':
+            resp.innerHTML = 'Trinta e Seis';
+            break;
+            case '37':
+            resp.innerHTML = 'Trinta e Sete';
+            break;
+            case '38':
+            resp.innerHTML = 'Trinta e Oito';
+            break;
+            case '39':
+            resp.innerHTML = 'Trinta e Nove';
+            break;
+            case '40':
+            resp.innerHTML = 'Quarenta';
+            break;
+            // de 41 a 50
+            case '41':
+            resp.innerHTML = 'Quarenta e Um';
+            break;
+            case '42':
+            resp.innerHTML = 'Quarenta e Dois';
+            break;
+            case '43':
+            resp.innerHTML = 'Quarenta e Três';
+            break;
+            case '44':
+            resp.innerHTML = 'Quarenta e Quatro';
+            break;
+            case '45':
+            resp.innerHTML = 'Quarenta e Cinco';
+            break;
+            case '46':
+            resp.innerHTML = 'Quarenta e Seis';
+            break;
+            case '47':
+            resp.innerHTML = 'Quarenta e Sete';
+            break;
+            case '48':
+            resp.innerHTML = 'Quarenta e Oito';
+            break;
+            case '49':
+            resp.innerHTML = 'Quarenta e Nove';
+            break;
+
+            // de 51 a 60
+            case '51':
+            resp.innerHTML = 'Cinquenta e Um';
+            break;
+            case '52':
+            resp.innerHTML = 'Cinquenta e Dois';
+            break;
+            case '53':
+            resp.innerHTML = 'Cinquenta e Três';
+            break;
+            case '54':
+            resp.innerHTML = 'Cinquenta e Quatro';
+            break;
+            case '55':
+            resp.innerHTML = 'Cinquenta e Cinco';
+            break;
+            case '56':
+            resp.innerHTML = 'Cinquenta e Seis';
+            break;
+            case '57':
+            resp.innerHTML = 'Cinquenta e Sete';
+            break;
+            case '58':
+            resp.innerHTML = 'Cinquenta e Oito';
+            break;
+            case '59':
+            resp.innerHTML = 'Cinquenta e Nove';
+            break;
+            case '60':
+            resp.innerHTML = 'Sessenta';
+            break;
+                
+            //de 61 a 70
+            case '61':
+            resp.innerHTML = 'Sessenta e Um';
+            break;
+            case '62':
+                resp.innerHTML = 'Sessenta e Dois';
+                break;
+            case '63':
+                    resp.innerHTML = 'Sessenta e Três';
+                    break;
+            case '64':
+                        resp.innerHTML = 'Sessenta e Quatro';
+                        break;
+            case '65':
+                            resp.innerHTML = 'Sessenta e Cinco';
+                            break;
+            case '66':
+                                resp.innerHTML = 'Sessenta e Seis';
+                                break;
+            case '67':
+                                    resp.innerHTML = 'Sessenta e Sete';
+                                    break;
+            case '68':
+                                        resp.innerHTML = 'Sessenta e Oito';
+                                        break;
+            case '69':
+                                            resp.innerHTML = 'Sessenta e Nove';
+                                            break;
+            case '70':
+                                                resp.innerHTML = 'Setenta';
+                                                break;
+    
+         //de 71 a 80
+            case '71':
+            resp.innerHTML = 'Setenta e Um';
+            break;
+            case '72':
+            resp.innerHTML = 'Setenta e Dois';
+            break;
+            case '73':
+            resp.innerHTML = 'Setenta e Três';
+            break;
+            case '74':
+            resp.innerHTML = 'Setenta e Quatro';
+            break;
+            case '75':
+            resp.innerHTML = 'Setenta e Cinco';
+            break;
+            case '76':
+            resp.innerHTML = 'Setenta e Seis';
+            break;
+            case '77':
+            resp.innerHTML = 'Setenta e Sete';
+            break;
+            case '78':
+            resp.innerHTML = 'Setenta e Oito';
+            break;
+            case '79':
+            resp.innerHTML = 'Setenta e Nove';
+            break;
+            case '80':
+            resp.innerHTML = 'Oitenta';
+            break;
+         // de 81 a 90
+            case '81':
+            resp.innerHTML = 'Oitenta e Um';
+            break;
+            case '82':
+            resp.innerHTML = 'Oitenta e Dois';
+            break;
+            case '83':
+            resp.innerHTML = 'Oitenta e Três';
+            break;
+            case '84':
+            resp.innerHTML = 'Oitenta e Quatro';
+            break;
+            case '85':
+            resp.innerHTML = 'Oitenta e Cinco';
+            break;
+            case '86':
+            resp.innerHTML = 'Oitenta e Seis';
+            break;
+            case '87':
+            resp.innerHTML = 'Oitenta e Sete';
+            break;
+            case '88':
+            resp.innerHTML = 'Oitenta e Oito';
+            break;
+            case '89':
+            resp.innerHTML = 'Oitenta e Nove';
+            break;
+            case '90':
+            resp.innerHTML = 'Noventa';
+            break;
+         //de 91 a 99
+            case '91':
+            resp.innerHTML = 'Noventa e Um';
+            break;
+            case '92':
+            resp.innerHTML = 'Noventa e Dois';
+            break;
+            case '93':
+            resp.innerHTML = 'Noventa e Três';
+            break;
+            case '94':
+            resp.innerHTML = 'Noventa e Quatro';
+            break;
+            case '95':
+            resp.innerHTML = 'Noventa e Cinco';
+            break;
+            case '96':
+            resp.innerHTML = 'Noventa e Seis';
+            break;
+            case '97':
+            resp.innerHTML = 'Noventa e Sete';
+            break;
+            case '98':
+            resp.innerHTML = 'Noventa e Oito';
+            break;
+            case '99':
+            resp.innerHTML = 'Noventa e Nove';
+            break;
+    } 
+
+    if(numero>99){
+        resp.innerHTML = 'Digite apenas dois algarismos no máximo'
+    }
+}
